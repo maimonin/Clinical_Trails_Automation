6179da4d27dc84529ec6334fecc6805f0f9d480d
maimonin <48507194+maimonin@users.noreply.github.com>
2021-12-20 18:55:09 +0200

    fixed

--- Engine/Nodes.py
+++ Engine/Nodes.py
@@ -1,6 +1,8 @@
+import threading
 from abc import ABC, abstractmethod
 from typing import List
 from Engine import Data, Server
+from Engine.Server import log
 from Engine.User import User
 
 node_id = 0
@@ -22,14 +24,19 @@ class Node(ABC):
     @abstractmethod
     def exec(self) -> None:
         pass
+    @abstractmethod
+    def has_actors(self):
+        pass
 
 
 class DataEntering(Node):
-    def __init__(self, role, next_node):
+    def __init__(self, role, next_node, titly):
         global node_id
         self.id = node_id
+        self.title=titly
         self.role = role
         self.next = next_node
+        self.lock= threading.Lock()
         node_id += 1
 
     participants: List[User] = []
@@ -46,30 +53,35 @@ class DataEntering(Node):
         self.next.exec()
 
     def notify(self) -> None:
-        print("Hello: Notifying observers...")
-        for participant in self.participants:
+        self.lock.acquire()
+        participants2=self.participants.copy()
+        self.participants=[]
+        self.lock.release()
+        for participant in participants2:
+            log("participant id"+ participant.id + " in data entering node with title: " + self.title)
             if self.role == "participant":
                 # ask server to send request to actors and receive answers
                 results = participant.update(lambda: print("I'm a participant"))
                 Data.add_data(results, participant)
-                self.detach(participant)
                 self.next.attach(participant)
             else:
                 actor = Server.get_role(self.role)
                 # ask server to send request to actors and receive answers
                 results = actor.update(lambda: print("I'm a "+self.role))
                 Data.add_data(results, participant)
-                self.detach(participant)
                 self.next.attach(participant)
 
+    def has_actors(self):
+        return len(self.participants) != 0
 
 class Decision(Node):
-    def __init__(self, role, next_options, condition):
+    def __init__(self,next_options, condition, titly):
         global node_id
         self.id = node_id
-        self.role = role
+        self.title=titly
         self.nexts = next_options
         self.condition = condition
+        self.lock= threading.Lock()
         node_id += 1
 
     participants: List[User] = []
@@ -83,24 +95,24 @@ class Decision(Node):
 
     def exec(self) -> None:
         self.notify()
-        if condition():
+        if self.nexts[0].has_actors():
             self.nexts[0].exec()
-        else:
-            self.next[1].exec()
+        if self.nexts[1].has_actors():
+            self.nexts[1].exec()
+
+    def has_actors(self):
+        return len(self.participants)==0
 
     def notify(self) -> None:
         print("Decision: Notifying observers...")
-        for participant in self.participants:
-            if self.role == "participant":
-                # ask server to send request to actors and receive answers
-                results = participant.update(lambda: print("I'm a participant"))
-                Data.add_data(results, participant)
-                self.detach(participant)
-                self.next.attach(participant)
+        self.lock.acquire()
+        participants2 = self.participants.copy()
+        self.participants = []
+        self.lock.release()
+        for participant in participants2:
+            log("participant id" + participant.id + "in decision node with title: " + self.title)
+            self.detach(participant)
+            if self.condition(participant):
+                self.nexts[0].attach(participant)
             else:
-                actor = Server.get_role(self.role)
-                # ask server to send request to actors and receive answers
-                results = actor.update(lambda: print("I'm a "+self.role))
-                Data.add_data(results, participant)
-                self.detach(participant)
-                self.next.attach(participant)
+                self.nexts[1].attach(participant)
