6d91a723956b66982d4a7fa4c524f5ea8e1478fc
inbar maimon <maimonin@post.bgu.ac.il>
2021-12-23 11:49:10 +0200

    "string node parse"

--- Engine/Server.py
+++ Engine/Server.py
@@ -8,7 +8,10 @@ from Users import add_user
 
 workflows = {}
 print_lock = threading.Lock()
-
+OP_NODE_QUESTIONNAIRE = 1
+OP_NODE_DATA_ENTRY = 2
+OP_NODE_DECISION = 3
+OP_NODE_STRING = 4
 
 def parse_Questionnaire(node_dict):
     content = node_dict['content']
@@ -23,6 +26,62 @@ def parse_Test(node_dict):
     node = DataEntering(node_dict['id'], node_details['title'], node_details['actor in charge'], content['tests'])
     return node
 
+def parse_Decision(node_dict):
+    content = node_dict['content']
+    node_details = content['node_details']
+    node = DataEntering(node_dict['id'], node_details['title'], node_details['actor in charge'], content['tests'])
+    return node
+
+def parse_String_Node(node_dict):
+    content = node_dict['content']
+    node_details = content['node_details']
+    node = DataEntering(node_dict['id'], node_details['title'], node_details['actors'], content['text'])
+    return node
+
+def register_user(self, user_dict, c):
+    add_user(user_dict['role'], user_dict['id'], c)
+    log("user " + user_dict['role'] + " received")
+    connected = json.dumps({'type': 'connect', 'id': user_dict['id']})
+    c.send(connected.encode('ascii'))
+    if user_dict['role'] == "participant":
+        if len(workflows) == 0:
+            print("No workflow yet")
+            c.close()
+        else:
+            # start participant's workflow
+            add_user(user_dict['role'], user_dict['id'], c)
+            workflows[user_dict["workflow"]].attach(user_dict['id'])
+            workflows[user_dict["workflow"]].exec()
+
+
+def new_workflow(self, data_dict, c):
+    nodes = {}
+    outputs = {}
+    inputs = {}
+    print(data_dict)
+    for node in data_dict['nodes']:
+        if node['op_code'] == OP_NODE_QUESTIONNAIRE:
+            nodes[node['id']] = parse_Questionnaire(node)
+        elif node['op_code'] == OP_NODE_DATA_ENTRY:
+            nodes[node['id']] = parse_Test(node)
+        elif node['op_code'] == OP_NODE_DECISION:
+            nodes[node['id']] = parse_Decision(node)
+        elif node['op_code'] == OP_NODE_STRING:
+            nodes[node['id']] = parse_String_Node(node)
+        for out in node['outputs']:
+            outputs[out['id']] = node['id']
+        for inp in node['inputs']:
+            inputs[inp['id']] = node['id']
+    first_node = data_dict['nodes'][0]['id']
+    workflows[data_dict["workflow_id"]] = nodes[first_node]
+    for edge in data_dict['edges']:
+        first_id = outputs[edge['start']]
+        second_id = inputs[edge['end']]
+        first = nodes[first_id]
+        second = nodes[second_id]
+        # label= edge['label']
+        first.next = second
+
 
 def threaded(c):
     global workflows
@@ -35,45 +94,9 @@ def threaded(c):
 
         data_dict = json.loads(data)
         if data_dict['sender'] == 'simulator':
-            user_dict = data_dict
-            add_user(user_dict['role'], user_dict['id'], c)
-            log("user " + user_dict['role'] + " received")
-            connected = json.dumps({'type': 'connect', 'id': user_dict['id']})
-            c.send(connected.encode('ascii'))
-            if user_dict['role'] == "participant":
-                if len(workflows) == 0:
-                    print("No workflow yet")
-                    c.close()
-                else:
-                    # start participant's workflow
-                    add_user(user_dict['role'], user_dict['id'], c)
-                    workflows[user_dict["workflow"]].attach(user_dict['id'])
-                    workflows[user_dict["workflow"]].exec()
+            register_user(data_dict, c)
         else:
-            nodes = {}
-            outputs = {}
-            inputs = {}
-            print(data_dict)
-            for node in data_dict['nodes']:
-                if node['title'] == 'Questionnaire':
-                    nodes[node['id']] = parse_Questionnaire(node)
-                elif node['title'] == 'Data Entry':
-                    nodes[node['id']] = parse_Test(node)
-                if node['title'] != 'Decision':
-                    for out in node['outputs']:
-                        outputs[out['id']] = node['id']
-                    for inp in node['inputs']:
-                        inputs[inp['id']] = node['id']
-            first_node = data_dict['nodes'][0]['id']
-            workflows[data_dict["workflow_id"]] = nodes[first_node]
-            for edge in data_dict['edges']:
-                first_id = outputs[edge['start']]
-                second_id = inputs[edge['end']]
-                first = nodes[first_id]
-                second = nodes[second_id]
-                # label= edge['label']
-                first.next = second
-
+            new_workflow(data_dict, c)
             break
 
     c.close()
