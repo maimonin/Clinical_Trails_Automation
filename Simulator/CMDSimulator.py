1e2b2d82ba3d1dffc0c7f6a41a785e62eb47b9b5
maimonin <48507194+maimonin@users.noreply.github.com>
2021-12-29 11:28:23 +0200

    cmd simulator

--- Simulator/CMDSimulator.py
+++ Simulator/CMDSimulator.py
@@ -27,34 +27,34 @@ lock = threading.Lock()
 
 
 def handel_open_question(q):
-    return {"question":q,"answer":input(q['text'])}
+    return {"question": q, "answer": input(q['text'])}
 
 
 def handel_radio_question(q):
     print(q['text'])
-    i=0
+    i = 0
     for opt in q['options']:
         print(str(i) + ". " + opt)
-        i+=1
+        i += 1
     val = input("type answer number:")
     val = int(val)
-    return {"question":q,"answer":q['options'][val]}
+    return {"question": q, "answer": q['options'][val]}
 
 
 def handel_multi_question(q):
     ans = []
     print(q['text'])
-    i=0
+    i = 0
     for opt in q['options']:
         print(str(i) + ". " + opt)
-        i+=1
+        i += 1
     val = input("type answer number:")
     while val != -1:
         val = input("type answer number or -1 to finish:")
-        val=int(val)
+        val = int(val)
         if val != -1:
             ans.append(q['options'][val])
-    return {"question":q,"answer":ans}
+    return {"question": q, "answer": ans}
 
 
 def handel_questionnaire(questions, participant):
@@ -72,33 +72,31 @@ def handel_questionnaire(questions, participant):
     return answers
 
 
-
-def actor_simulation(user):
+def actor_simulation(user,s):
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((host, port))
-        register_user(user, s)
         while True:
             data = s.recv(5000)
             print(data)
             data_json = json.loads(data)
             if data_json['type'] == 'notification':
                 lock.acquire()
-                print(user["name"]+"got notification: "+data_json['text'])
+                print(user["name"] + "got notification: " + data_json['text'])
                 lock.release()
             if data_json['type'] == 'questionnaire':
-                ans = handel_questionnaire(data_json['questions'],user)
+                ans = handel_questionnaire(data_json['questions'], user)
                 s.send(json.dumps({"answers": ans}).encode('ascii'))
             elif data_json['type'] == 'test':
                 lock.acquire()
-                print(f"{user['name']}:  patient with id {data_json['patient']} is about to arrive please take test: {data_json['name']}  \n"
-                      f"the instructions for this test are: {data_json['instructions']}")
+                print(
+                    f"{user['name']}:  patient with id {data_json['patient']} is about to arrive please take test: {data_json['name']}  \n"
+                    f"the instructions for this test are: {data_json['instructions']}")
                 lock.release()
             elif data_json['type'] == 'test data entry':
                 lock.acquire()
-                val=input(f"{user['name']}:  patient with id {data_json['patient']} is about to arrive please take test: {data_json['name']}  \n")
+                val = input(
+                    f"{user['name']}:  patient with id {data_json['patient']} is about to arrive please take test: {data_json['name']}  \n")
                 lock.release()
-                s.send(json.dumps({"test": data_json['name'], 'result':val}).encode('ascii'))
+                s.send(json.dumps({"test": data_json['name'], 'result': val}).encode('ascii'))
             elif data_json['type'] == 'terminate':
                 s.close()
                 break
@@ -119,7 +117,12 @@ def register_user(user, s):
 def Main():
     threads = []
     for user in users:
-        threads.append(threading.Thread(target=actor_simulation, args=(user,)))
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        register_user(user, s)
+        user['s'] = s
+    for user in users:
+        threads.append(threading.Thread(target=actor_simulation, args=(user,user['s'])))
     for t in threads:
         t.start()
     for t in threads:
