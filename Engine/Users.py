9eb1bab118e1757df653f25cb927c612c5a1c3c0
romibarel <romib19999@gmail.com>
2022-01-03 21:58:56 +0200

    started sched

--- Engine/Users.py
+++ Engine/Users.py
@@ -1,7 +1,6 @@
 import json
 import time
-from random import randint
-
+import Scheduler
 from Logger import log
 import user_lists
 
@@ -21,45 +20,6 @@ class User:
         return {"gender": self.sex, "age": self.age}
 
 
-def add_user(role, sex, age, user_id, socket):
-    user = User(role, sex, age, user_id, socket)
-    if role == "nurse":
-        user_lists.nurses.append(user)
-    elif role == "doctor":
-        user_lists.doctors.append(user)
-    elif role == "investigator":
-        user_lists.investigators.append(user)
-    elif role == "lab":
-        user_lists.labs.append(user)
-    elif role == "participant":
-        user_lists.participants[user.id] = user
-
-    return user
-
-
-def get_role(role):
-    if str(role).lower() == "nurse":
-        if len(user_lists.nurses) == 0:
-            print("no available nurse")
-            return None
-        return user_lists.nurses[randint(0, len(user_lists.nurses) - 1)]
-    if str(role).lower() == "doctor":
-        if len(user_lists.doctors) == 0:
-            print("no available doctor")
-            return None
-        return user_lists.doctors[randint(0, len(user_lists.doctors) - 1)]
-    if str(role).lower() == "investigator":
-        if len(user_lists.investigators) == 0:
-            print("no available investigator")
-            return None
-        return user_lists.investigators[randint(0, len(user_lists.investigators) - 1)]
-    if str(role).lower() == "lab":
-        if len(user_lists.labs) == 0:
-            print("no available lab worker")
-            return None
-        return user_lists.labs[randint(0, len(user_lists.labs) - 1)]
-
-
 def answer_questionnaire(questions, s):
     s.send(json.dumps({'type': 'questionnaire', 'questions': questions}).encode('ascii'))
     ans = s.recv(5000)
@@ -68,16 +28,19 @@ def answer_questionnaire(questions, s):
 
 
 # name, instructions, staff
-def take_test(user_id, test, in_charge, s):
+def take_test(user_id, test, remaining_time, in_charge, s):
     for role in test.staff:
-        get_role(role).socket.send(json.dumps({'type': 'test', 'name': test.name,
-                                               'instructions': test.instructions,
-                                               'patient': user_id}).encode('ascii'))
+        actor = Scheduler.get_role(role, remaining_time)
+        if actor is None:
+            return None
+        actor.socket.send(json.dumps({'type': 'test', 'name': test.name,
+                                      'instructions': test.instructions,
+                                      'patient': user_id}).encode('ascii'))
     s.send(json.dumps({'type': 'notification', 'text': "show up to " + test.name}).encode('ascii'))
     log("participant with id " + str(user_id) + " taking a test")
     time.sleep(int(test.duration))
     form = {'type': 'test data entry', 'test': test.to_json(), 'patient': user_id}
-    r = get_role(in_charge)
+    r = Scheduler.get_role(in_charge)
     r.socket.send(json.dumps(form).encode('ascii'))
     results = r.socket.recv(5000)
     return json.loads(results)
--- Engine/user_lists.py
+++ Engine/user_lists.py
@@ -1,3 +1,8 @@
+from random import randint
+
+from Users import User
+
+
 def init():
     global nurses
     global doctors
@@ -9,3 +14,42 @@ def init():
     investigators = []
     labs = []
     participants = {}
+
+
+def get_role(role):
+    if str(role).lower() == "nurse":
+        if len(nurses) == 0:
+            print("no available nurse")
+            return None
+        return nurses[randint(0, len(nurses) - 1)]
+    if str(role).lower() == "doctor":
+        if len(doctors) == 0:
+            print("no available doctor")
+            return None
+        return doctors[randint(0, len(doctors) - 1)]
+    if str(role).lower() == "investigator":
+        if len(investigators) == 0:
+            print("no available investigator")
+            return None
+        return investigators[randint(0, len(investigators) - 1)]
+    if str(role).lower() == "lab":
+        if len(labs) == 0:
+            print("no available lab worker")
+            return None
+        return labs[randint(0, len(labs) - 1)]
+    return None
+
+
+def add_user(role, sex, age, user_id, socket):
+    user = User(role, sex, age, user_id, socket)
+    if role == "nurse":
+        nurses.append(user)
+    elif role == "doctor":
+        doctors.append(user)
+    elif role == "investigator":
+        investigators.append(user)
+    elif role == "lab":
+        labs.append(user)
+    elif role == "participant":
+        participants[user.id] = user
+    return user
