9facdb834a868c344e4161025652b3831dc79308
yehnet <yehnet@post.bgu.ac.il>
2022-03-08 19:43:54 +0200

    pytest: add automated run

--- Workflow Editor/Tests/UITestTry.py
+++ Workflow Editor/Tests/UITestTry.py
@@ -2,6 +2,7 @@ import pytest
 from PyQt5 import QtCore
 import workflow_window
 
+
 @pytest.fixture
 def app(qtbot):
     test_try = workflow_window.WorkflowEditorWindow()
@@ -9,11 +10,18 @@ def app(qtbot):
 
     return test_try
 
+
 def test_attributes_title(app):
     assert app.attributes.windowTitle() == "Attributes"
+
 
 def test_nodes_title(app):
     assert app.items.windowTitle() == "Nodes"
 
+
 def test_nodes_items(app):
+    pass
+
+
+if __name__ == '__main__':
     pass
