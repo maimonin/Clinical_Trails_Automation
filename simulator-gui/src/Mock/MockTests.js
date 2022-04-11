f61946bb7716d8cb12cfc17f35f004d46f626267
Raviv <ravivgil@post.bgu.ac.il>
2022-04-11 09:35:25 +0300

    changes test_notification and test_data_entry, and adds connection and mocks

--- simulator-gui/src/Mock/MockTests.js
+++ simulator-gui/src/Mock/MockTests.js
@@ -1,10 +1,14 @@
-export const tests_1 ={
-    id:"1",
-    tests:[{
-        "name":"blood check","instruction":"Take the blood from blood vessel",
-    "Facility":"Room 123","Duration":"5 min"},
-    {
-        "name":"Weight check","instruction":"",
-    "Facility":"Room 123","Duration":"5 min"}
-]
+export const test_data_entry_1 ={
+    type:"test data entry",
+    test:{name: "hemoglobin", instructions: "take a red tube of blood",
+             staff: ["Nurse"], duration: "3"},
+    patient: 22
+
 }
+export const test_notification_1 = {
+    type: "test",
+     name: "hemoglobin",
+     instructions: "take a red tube of blood",
+     patient: 22
+    }
+    
