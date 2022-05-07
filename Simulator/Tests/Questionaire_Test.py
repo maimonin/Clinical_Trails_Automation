6fe928ca54c9f44ad297f6c22961d4d4b5bbab60
romibarel <romib19999@gmail.com>
2022-05-07 14:59:17 +0300

    working on tests

--- Simulator/Tests/Questionaire_Test.py
+++ Simulator/Tests/Questionaire_Test.py
@@ -1,11 +1,17 @@
 import asyncio
-
 import tests_sim
+from Database import Database
 
 
 async def test():
-    output = await tests_sim.run('jsons/questionnaire_example.json', 'Answers/questionnaire_example_answers1.json',
-                                 1, 'female', 20)
-    print(output)
-    assert len(output) == 1
+    Database.delete_db(r"C:\Users\Owner\Desktop\Clinical_Trails_Automation\Engine\Database\test_data.db")
+    await tests_sim.run('jsons/questionnaire_example.json', 'Answers/questionnaire_example_answers1.json',
+                        1, 'female', 20)
+    print(Database.getUser(1))
+    assert Database.getUser(1).role == "participant", "user sghoulf be participant"
+    assert Database.getUser(1).sex == "female", "user should be fdemale"
+    assert Database.getUser(1).age == 20, "usere shoudl be 20"
+    print("passed")
+
+
 asyncio.run(test())
