8070caec815c10138603b75f7e3f8b81c1ee27a2
yehnet <yehnet@post.bgu.ac.il>
2022-05-25 10:37:45 +0300

    Revert "Nodes socket location."
    This reverts commit f4f7dcbb8ddf7515eb73038a38aa1676391ae80b.

--- Workflow Editor/workflow_graphics_socket.py
+++ Workflow Editor/workflow_graphics_socket.py
@@ -7,8 +7,6 @@ RIGHT = 0
 LEFT = 1
 DOWN = 2
 UP = 3
-
-
 class WFGraphicsSocket(QDMGraphicsSocket):
     def __init__(self, socket: 'Socket'):
         super().__init__(socket)
@@ -17,7 +15,6 @@ class WFGraphicsSocket(QDMGraphicsSocket):
             self.orientation = LEFT
         else:
             self.orientation = RIGHT
-
     def paint(self, painter, QStyleOptionGraphicsItem, widget=None):
         painter.setBrush(self._brush)
         painter.setPen(self._pen if not self.isHighlighted else self._pen_highlight)
@@ -27,25 +24,16 @@ class WFGraphicsSocket(QDMGraphicsSocket):
 
         painter.setRenderHint(QPainter.Antialiasing)
 
-        # FIXME: fix start and finish sockets location
-        # TODO: change edge exiting\entering point
-        fix_height = 12
-
         if self.orientation is RIGHT:
-            # path.lineTo(-self.width, self.width)
-            # path.lineTo(-self.width, -self.width)
-            # path.lineTo(0, 0)
-            path.moveTo(0, fix_height)
-            path.lineTo(-self.width, self.width + fix_height)
-            path.lineTo(-self.width, -self.width + fix_height)
-            path.lineTo(0, fix_height)
+            path.lineTo(-self.width, self.width)
+            path.lineTo(-self.width, -self.width)
+            path.lineTo(0, 0)
 
         elif self.orientation is LEFT:
-            path.moveTo(0, fix_height)
-            path.lineTo(0, -self.width + fix_height)
-            path.lineTo(self.width, fix_height)
-            path.lineTo(0, self.width + fix_height)
-            path.lineTo(0, fix_height)
+            path.lineTo(0, -self.width)
+            path.lineTo(self.width, 0)
+            path.lineTo(0, self.width)
+            path.lineTo(0, 0)
         # elif self.orientation is DOWN:
         #     path.lineTo(-self.width, self.width)
         #     path.lineTo(+self.width, +self.width)
@@ -56,20 +44,18 @@ class WFGraphicsSocket(QDMGraphicsSocket):
         #     path.lineTo(0, 0)
 
         painter.drawPath(path)
-
     def boundingRect(self) -> QRectF:
         """Defining Qt' bounding rectangle"""
-        return QRectF(QPoint(- self.width, -self.width), QPoint(self.width, self.width))
-
+        return QRectF(QPoint(- self.width,-self.width),QPoint(self.width,self.width))
 
 class WFGraphicsSocketDecision(WFGraphicsSocket):
     def __init__(self, socket: 'Socket'):
         super().__init__(socket)
         self.orientation = RIGHT
 
-    def change_orientation(self, orientation):
-        self.orientation = orientation
 
+    def change_orientation(self,orientation):
+        self.orientation = orientation
     def paint(self, painter, QStyleOptionGraphicsItem, widget=None):
         painter.setBrush(self._brush)
         painter.setPen(self._pen if not self.isHighlighted else self._pen_highlight)
@@ -101,7 +87,6 @@ class WFGraphicsSocketDecision(WFGraphicsSocket):
             path.lineTo(0, 0)
 
         painter.drawPath(path)
-
     def boundingRect(self) -> QRectF:
         """Defining Qt' bounding rectangle"""
-        return QRectF(QPoint(- self.width, -self.width), QPoint(self.width, self.width))
+        return QRectF(QPoint(- self.width,-self.width),QPoint(self.width,self.width))
